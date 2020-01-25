197c9c043522d7e8f57389aaa068dd47cf528fbf
psktcsharp <safeerlibya@gmail.com>
2020-01-25 19:45:35 +0100

    wrote models for service and subscription to test many to many relationship

---
+++ AspNetCoreGraph/Models/Service.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace AspNetCoreGraph.Models
+{
+    public class Service
+    {
+        public Service(){
+        this.Subscriptions = new HashSet<Subscription>();
+        }
+          public int Id { get; set; }
+        public virtual ICollection<Subscription> Subscriptions { get; set; }
+    }
+}
---
+++ AspNetCoreGraph/Models/Subscription.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+
+namespace AspNetCoreGraph.Models
+{
+    public class Subscription
+    {
+        public Subscription(){
+        this.Services = new HashSet<Service>();        
+        }
+        public int Id { get; set; }
+        public virtual ICollection<Service> Services { get; set; }
+    }
+}
